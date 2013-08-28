e9299c01f8b7ac0a7577e428034562d6ffc1ee88
stephanbruny <stephan.bruny@live.de>
2013-08-28 16:54:18 +0200

    ~ fixed strange git stuff

--- metro_tetris_master/src/scenes/singleplayer.lua
+++ metro_tetris_master/src/scenes/singleplayer.lua
@@ -53,24 +53,6 @@ end
 function SingleplayerScene.onLoad(conf)
 	field = Field.new(field_width, field_height);
 	print(Tserial.pack(field));
-=======
-local function start_game()
-	state = 2;
-	current_block = Stone.new(field);
-	next_block = Stone.new(field);
-end
-
-local function blink_ready()
-	local a = 0;
-	
-	if (blink.alpha == 255) then a = 0 else a = 255; end
-	
-	tween.start(1, blink, {alpha = a}, "linear", function() blink_ready() end)
-end
-
-function SingleplayerScene.onLoad(conf)
-	field = Field.new(field_width, field_height);
->>>>>>> a84870f093b5153313392435695c3882cd455895
 	offsetX = love.graphics.getWidth() / 2 - (field_width / 2 * block_size) - block_size;
 	offsetY = love.graphics.getHeight() / 2 - (field_height / 2 * block_size) - block_size;
 	blink_ready();
@@ -78,7 +60,6 @@ function SingleplayerScene.onLoad(conf)
 end
 
 function SingleplayerScene.onDraw()
-<<<<<<< HEAD
 	-- Field.draw(field, 32, offsetX, offsetY);
 	
 	for i = 1, field.width do
@@ -99,9 +80,6 @@ function SingleplayerScene.onDraw()
 			end
 		end
 	end
-=======
-	Field.draw(field, 32, offsetX, offsetY);
->>>>>>> a84870f093b5153313392435695c3882cd455895
 	
 	love.graphics.setColor(0,0,255,200);
 	love.graphics.setLine(2, "smooth");
@@ -112,11 +90,7 @@ function SingleplayerScene.onDraw()
 		love.graphics.printf("Ready?", love.graphics.getWidth() / 2, love.graphics.getHeight() / 2 - 24, 0, "center", 200)
 	end	
 	
-<<<<<<< HEAD
 	if (state >= 2 and state <= 4) then
-=======
-	if (state == 2) then
->>>>>>> a84870f093b5153313392435695c3882cd455895
 		if (current_block ~= nil) then
 			local offsetBlockX = offsetX + current_block.x * block_size;
 			local offsetBlockY = offsetY + current_block.y * block_size;
@@ -127,18 +101,12 @@ function SingleplayerScene.onDraw()
 						love.graphics.setColor(current_block.color.r, current_block.color.g, current_block.color.b, 255);
 						love.graphics.rectangle("fill", offsetBlockX + x * block_size, offsetBlockY + y *  block_size,  block_size,  block_size )
 					end
-<<<<<<< HEAD
 					
 				end
 			end
 		end
 		love.graphics.setColor(255,255,255,255);
 		love.graphics.print("Score: " .. player_score, 5, 5);
-=======
-				end
-			end
-		end
->>>>>>> a84870f093b5153313392435695c3882cd455895
 	end
 end
 
@@ -168,15 +136,11 @@ function SingleplayerScene.onUpdate(dt)
 				for k,v in pairs(current_block.color) do
 					origCol[k] = v;
 				end 
-<<<<<<< HEAD
 				state = 3;
-=======
->>>>>>> a84870f093b5153313392435695c3882cd455895
 				tween.start(0.05, current_block.color, {r = 255, b = 255, g = 255}, "inQuad", function() 
 					tween.start(0.05, current_block.color, {r = origCol.r, b = origCol.b, g = origCol.g}, "inQuad", function() 
 						-- make solid
 						make_solid(current_block)
-<<<<<<< HEAD
 						local rows = 0;
 						while Field.check_rows(field) do rows = rows +1; end
 						if (rows > 0) then
@@ -185,9 +149,6 @@ function SingleplayerScene.onUpdate(dt)
 						end
 						create_next_block();
 						state = 2;
-=======
-						create_next_block();
->>>>>>> a84870f093b5153313392435695c3882cd455895
 					end);
 				end) 
 			end);
@@ -202,7 +163,6 @@ function SingleplayerScene.onKeypressed(key)
 			start_game(); 
 		end;
 	end
-<<<<<<< HEAD
 	
 	if (state == 2) then
 		if (current_block ~= nil) then
@@ -223,8 +183,6 @@ function SingleplayerScene.onKeypressed(key)
 			end
 		end
 	end
-=======
->>>>>>> a84870f093b5153313392435695c3882cd455895
 end
 
 function SingleplayerScene.onJoystickpressed(joy, key)
